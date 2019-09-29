5acc50680045c57c69efa963a3ec088a268b8cbb
Christian Nygaard <christian@cnygaard.com>
2019-09-29 11:22:48 +0200

    * Initial Commit of Blog code in Java VertX Web

---
+++ src/main/java/blog.java
@@ -0,0 +1,19 @@
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
+public class blog {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        HttpServer server = vertx.createHttpServer();
+//        Router router = Router.router(vertx);
+
+        server.requestHandler(request -> {
+            HttpServerResponse response = request.response();
+            response.putHeader("content-type", "text-plain");
+            response.end("Hello World!");
+        });
+        server.listen(8080);
+    }
+}
